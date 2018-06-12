278151fede9cf2311cbca8a4e1a154e5bca91587
Saiful Islam Rasel <s.i.r.computerengineer.24@gmail.com>
2018-06-12 18:39:16 +0600

    Hiararicical inheritance added and multiple inheritance modified

--- hiararcical_inheritance.cpp
+++ hiararcical_inheritance.cpp
@@ -1,4 +1,4 @@
-/*  Program that describe multiple inheritance
+/*  Program that describe hiararcical inheritance
     Author : Saiful Islam Rasel
 */
 
@@ -21,19 +21,7 @@ public:
     void getpersoninformation();
 };
 
-class School{
-    string schoolname;
-    string Department;
-protected:
-    void setschoolname(string schoolname);
-    void setDepartment(string Department);
-public:
-    School();
-    void setschoolinformation(string schoolname,string Department);
-    void getschoolinformation();
-};
-
-class Student : public Person,public School{
+class Student : public Person{
     string studentid;
     double cgpa;
 protected:
@@ -41,8 +29,20 @@ protected:
     void setstudentcgpa(double cgpa);
 public:
     Student();
-    void setstudentinformation(string name,string id,int age,string schoolname,string Department,string studentid,double cgpa);
+    void setstudentinformation(string name,string id,int age, string studentid,double cgpa);
     void getstudentinformation();
+};
+
+class Worker : public Person{
+    string workerid;
+    int salary;
+protected:
+    void setworkerid(string workerid);
+    void setworkersalary(int salary);
+public:
+    Worker();
+    void setworkerinformation(string name,string id,int age,string workerid,int salary);
+    void getworkerinformation();
 };
 
 Person::Person(){
@@ -75,38 +75,13 @@ void Person::getpersoninformation(){
     cout<<"Age : "<<age<<endl;
 }
 
-School::School(){
-    schoolname = "unknown";
-    Department = "unknown";
-}
-
-void School::setschoolinformation(string schoolname,string Department){
-    setschoolname(schoolname);
-    setDepartment(Department);
-}
-
-void School::setschoolname(string schoolname){
-    this -> schoolname = schoolname;
-}
-
-void School::setDepartment(string Department){
-    this -> Department = Department;
-}
-
-void School::getschoolinformation(){
-    cout<<"School Name : "<<schoolname<<endl;
-    cout<<"section : "<<Department<<endl;
-}
-
-
 Student::Student(){
     studentid="Unknown";
     cgpa=0;
 }
 
-void Student::setstudentinformation(string name,string id,int age,string schoolname,string Department,string studentid,double cgpa){
+void Student::setstudentinformation(string name,string id,int age, string studentid,double cgpa){
     setpersoninformation(name,id,age);
-    setschoolinformation(schoolname,Department);
     setstudentid(id);
     setstudentcgpa(cgpa);
 }
@@ -121,12 +96,36 @@ void Student::setstudentcgpa(double cgpa){
 
 void Student::getstudentinformation(){
     getpersoninformation();
-    getschoolinformation();
     cout<<"Student ID: "<<studentid<<endl;
     cout<<"CGPA : "<<cgpa<<endl;
 }
 
-void getinformation(string & name,string & id,int & age,string & schoolname,string & Department,string & studentid,double & cgpa){
+Worker::Worker(){
+    workerid = "unknown";
+    salary = 0;
+}
+
+void Worker::setworkerinformation(string name,string id,int age,string workerid,int salary){
+    setpersoninformation(name,id,age);
+    setworkerid(workerid);
+    setworkersalary(salary);
+}
+
+void Worker::setworkerid(string workerid){
+    this -> workerid = workerid;
+}
+
+void Worker::setworkersalary(int salary){
+    this -> salary = salary;
+}
+
+void Worker::getworkerinformation(){
+    getpersoninformation();
+    cout<<"Worker Id : "<<workerid<<endl;
+    cout<<"Worker Salary : "<<salary<<endl;
+}
+
+void getinformation(string & name,string & id,int & age, string & studentid, double & cgpa){
     cout<<"Enter Student information: "<<endl;
     cout<<"Name: ";
     getline(cin,name);
@@ -134,26 +133,44 @@ void getinformation(string & name,string & id,int & age,string & schoolname,stri
     cin>>id;
     cout<<"Age : ";
     cin>>age;
-    getchar();
-    cout<<"School Name: ";
-    getline(cin,schoolname);
-    cout<<"Department ; ";
-    cin>>Department;
     cout<<"Student ID : ";
     cin>>studentid;
     cout<<"CGPA : ";
     cin>>cgpa;
+    getchar();
+}
+
+void getinformation(string & name,string &id,int & age, string & workerid, int & salary){
+    cout<<"Enter Worker information: "<<endl;
+    cout<<"Name: ";
+    getline(cin,name);
+    cout<<"Id : ";
+    cin>>id;
+    cout<<"Age : ";
+    cin>>age;
+    cout<<"Worker ID : ";
+    cin>>workerid;
+    cout<<"Salary : ";
+    cin>>salary;
 }
 
 int main(){
     Student a;
-    string name,id,studentid,schoolname,Department;
+    string name,id,studentid;
     int age;
     double cgpa;
-    getinformation(name,id,age,schoolname,Department,studentid,cgpa);
+    getinformation(name,id,age,studentid,cgpa);
     cout<<endl;
-    a.setstudentinformation(name,id,age,schoolname,Department,studentid,cgpa);
+    a.setstudentinformation(name,id,age,studentid,cgpa);
     a.getstudentinformation();
+    cout<<endl;
+    Worker b;
+    string workerid;
+    int salary;
+    getinformation(name,id,age,workerid,salary);
+    cout<<endl;
+    b.setworkerinformation(name,id,age,workerid,salary);
+    b.getworkerinformation();
     cout<<endl;
     return 0;
 }
